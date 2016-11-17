f63535bae62a216bc5f56c0e1ce924dd42d3a72d
Tom Thorley <tomthorley87@gmail.com>
2016-11-17 19:53:15 +0000

    update to sass

--- wp-content/themes/storefront-child/gulpfile.js
+++ wp-content/themes/storefront-child/gulpfile.js
@@ -26,7 +26,7 @@ gulp.task('sass', function() {
 	return sass('assets/sass/style.scss', { style: 'expanded' })
 		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
 		.pipe(minifycss())
-		.pipe(gulp.dest('/'))
+		.pipe(gulp.dest('dist/css/'))
 		//.pipe(browserSync.stream())
 		.pipe(notify({ message: 'Sass completed' }));
 });
